9da927145867d6dbca84f1f4397fcd69e2df7edc
priegoomar <161537962+priegoomar@users.noreply.github.com>
2026-06-15 22:47:36 -0600

    app.py

--- app.py
+++ app.py
@@ -242,8 +242,8 @@ with tab_ingenieria:
     f_cod.append("abc")
     df_doe = pd.DataFrame()
     df_doe['Corrida'] = corridas
-    df_doe['Temp'] = f_tempd
-    f_doe['Enzima'] = f_enzd
-    f_doe['Tiempo'] = f_tied
-    f_doe['Codigo'] = f_cod
+    df_doe['Temp'] = f_temp
+    df_doe['Enzima'] = f_enz
+    df_doe['Tiempo'] = f_tie
+    df_doe['Codigo'] = f_cod
     st.dataframe(df_doe)
