6731ee39028b55a7c774acc73481fd7f73989306
priegoomar <161537962+priegoomar@users.noreply.github.com>
2026-06-16 21:49:00 -0600

    Update app.py

--- app.py
+++ app.py
@@ -127,8 +127,8 @@ if st.session_state["menu_activo"] == "🏠 Dashboard":
         st.caption("Panel Ejecutivo de Cribado para Cáncer de Mama en Etapa Temprana")
         st.markdown("---")
     
-    st.markdown('<div class="executive-card">', unsafe_allow_html=True)
-    st.markdown('<p class="card-heading">📥 Patient Case Enrollment Matrix</p>', unsafe_allow_html=True)
+        st.markdown('<div class="executive-card">', unsafe_allow_html=True)
+        st.markdown('<p class="card-heading">📥 Patient Case Enrollment Matrix</p>', unsafe_allow_html=True)
     
     col_f1, col_f2, col_f3 = st.columns(3)
     with col_f1: patient_id = st.text_input("🆔 Patient Identifier", placeholder="Ej. METH-2026-0X")
