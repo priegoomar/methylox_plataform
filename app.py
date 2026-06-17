a3f4c993bc905c780fe89307610ecac7a9b14814
priegoomar <161537962+priegoomar@users.noreply.github.com>
2026-06-17 15:42:03 -0600

    Update app.py

--- app.py
+++ app.py
@@ -123,6 +123,7 @@ if st.session_state["menu_activo"] == "🏠 Dashboard":
     st.image("banner_real.png", use_container_width=True, output_format="PNG")
     st.write("")
     st.write("")
+    
     col_f1, col_f2, col_f3 = st.columns(3)
     with col_f1:
         patient_id = st.text_input("🆔 Patient Identifier", placeholder="Ej. METH-2026-0X")
