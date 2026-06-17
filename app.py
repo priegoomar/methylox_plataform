8281b64cf41be014e7bde0b18c23fef89457037e
priegoomar <161537962+priegoomar@users.noreply.github.com>
2026-06-17 13:53:39 -0600

    Update app.py

--- app.py
+++ app.py
@@ -119,6 +119,7 @@ st.sidebar.caption("© 2026 MethylOx™")
 # --- 4. CONTROL DE PANTALLAS ---
 
 if st.session_state["menu_activo"] == "🏠 Dashboard":
+    st-write (")
     st.image("https://githubusercontent.com", use_container_width=True)
 
     st.markdown('<div class="executive-card">', unsafe_allow_html=True)
