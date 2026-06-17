6ff1ffe0bbab0ee4f94fbdf8b09bb1da27c04091
priegoomar <161537962+priegoomar@users.noreply.github.com>
2026-06-16 22:22:41 -0600

    Update app.py

--- app.py
+++ app.py
@@ -114,8 +114,6 @@ st.sidebar.caption("© 2026 MethylOx™")
 # --- 4. CONTROL DE PANTALLAS ---
 
 if st.session_state["menu_activo"] == "🏠 Dashboard":
-        
-        # RENDERIZADO HTML LÍQUIDO DEL BANNER (Ocupa el 90% horizontal de la ventana)
         banner_html = '<img src="banner_real.png" style="width:100vw; max-height:140px; object-fit:cover; margin-top:-50px; border-bottom:4px solid #2563EB; box-shadow: 0 4px 12px rgba(37,99,235,0.15);">'
         st.markdown(banner_html, unsafe_allow_html=True)
         
