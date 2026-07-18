a0aae754c248817624a66e72e31eab1e5a4d7076
priegoomar <161537962+priegoomar@users.noreply.github.com>
2026-07-17 23:14:14 -0600

    Update app.py

--- app.py
+++ app.py
@@ -749,8 +749,8 @@ elif nav_selection == "Motor METHYLOX":
                     # 🔒 CORRECCIÓN SINTÁCTICA: Se inyectan los datos faltantes en la lista para evitar errores de compilación
                     df_filtrado = pd.DataFrame({
                         "Probe_ID": ["MOX-SG-01", "MOX-SG-07", "MOX-SG-12"],
-                        "Methylated_Intensity":,
-                        "Unmethylated_Intensity": [820, 760, 910]
+                        "Methylated_Intensity":, [820, 760, 910]
+                        "Unmethylated_Intensity": [100, 140, 20]
                     })
                 
                 # B) CÁLCULO MATEMÁTICO REAL: Beta = M / (M + U + 100)
