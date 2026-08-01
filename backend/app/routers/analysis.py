4f8989e0f3d54bfce10ec67258de7b39829033e3
priegoomar <161537962+priegoomar@users.noreply.github.com>
2026-08-01 14:27:26 -0600

    Update analysis.py

--- backend/app/routers/analysis.py
+++ backend/app/routers/analysis.py
@@ -66,7 +66,10 @@ def create_analysis(
 def get_sample_analysis(
     sample_id: int,
     db: Session = Depends(get_db),
-    current_user: TokenData = Depends(permissionGuard("analysis_read"))):
+    current_user: TokenData = Depends(
+        PermissionGuard("analysis_read")
+    )
+):
     results = (
         db.query(models.AnalysisResult)
         .filter(
