7788cf28557e9e2f6271b4b3bfd471fadb22d0a8
priegoomar <161537962+priegoomar@users.noreply.github.com>
2026-08-02 23:03:02 -0600

    Update app.py

--- app.py
+++ app.py
@@ -175,12 +175,14 @@ with st.sidebar:
                         )
                     
                         st.session_state.user_role = decoded_token.get("role", "cls").lower()
-                                                st.rerun()
-                                        except Exception as e:
-                                            pass
+                        st.rerun()
+                except Exception as e:
+                    pass
+    
     # ============================================================
     # ACTIVE SESSION
     # ============================================================
+    
     else:
         st.markdown(
             f"""
