692925312bcb366f3ba0a29264241e08242e1350
priegoomar <161537962+priegoomar@users.noreply.github.com>
2026-08-02 21:58:36 -0600

    Update auth.py

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -110,7 +110,7 @@ def login(
 
     return {
         "access_token": token,
-        "token_type": "bearer"
+        "token_type": "bearer",
         "role": user.role,
         "username": user.username
     }
