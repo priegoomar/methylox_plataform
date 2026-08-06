6c1135e13ed69f518301d8bc6511fd8e054887f9
priegoomar <161537962+priegoomar@users.noreply.github.com>
2026-08-06 16:37:51 -0600

    Update start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-echo "RESETTING MIGRATION STATE..."
-alembic stamp base
+
+echo "Running Alembic migrations..."
 alembic upgrade head
+
 echo "Starting METHYLOX API..."
 uvicorn app.main:app --host 0.0.0.0 --port 8000
