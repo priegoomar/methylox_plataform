bec25bb3f85b5df5dc198a77f9efdcc3dc8581a0
priegoomar <161537962+priegoomar@users.noreply.github.com>
2026-08-06 15:32:53 -0600

    Update 002_add_patient_hospital_id.py

--- backend/alembic/verisons/002_add_patient_hospital_id.py
+++ backend/alembic/verisons/002_add_patient_hospital_id.py
@@ -1,26 +1,11 @@
 from alembic import op
-import sqlalchemy as sa
-
-
 revision = "002_add_patient_hospital_id"
 down_revision = "001_add_hospital_isolation"
 branch_labels = None
 depends_on = None
 
-
 def upgrade():
-    op.add_column(
-        "patients",
-        sa.Column(
-            "hospital_id",
-            sa.Integer(),
-            nullable=True
-        )
-    )
-
+    pass
 
 def downgrade():
-    op.drop_column(
-        "patients",
-        "hospital_id"
-    )
+    pass
