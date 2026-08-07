b048fc8099e6af09e54b0526ed47bfc0aa6cc57f
priegoomar <161537962+priegoomar@users.noreply.github.com>
2026-08-06 21:15:01 -0600

    Create 003_fix_sample_hospital_id.py

---
+++ backend/alembic/versions/003_fix_sample_hospital_id.py
@@ -0,0 +1,51 @@
+from alembic import op
+import sqlalchemy as sa
+
+
+revision = "003_fix_samples_hospital_id"
+down_revision = "002_add_sample_hospital_id"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column(
+        "samples",
+        sa.Column(
+            "hospital_id",
+            sa.Integer(),
+            nullable=True
+        )
+    )
+
+    op.create_foreign_key(
+        "fk_samples_hospital",
+        "samples",
+        "hospitals",
+        ["hospital_id"],
+        ["id"]
+    )
+
+    op.execute(
+        """
+        UPDATE samples
+        SET hospital_id = (
+            SELECT patients.hospital_id
+            FROM patients
+            WHERE patients.id = samples.patient_id
+        )
+        """
+    )
+
+
+def downgrade():
+    op.drop_constraint(
+        "fk_samples_hospital",
+        "samples",
+        type_="foreignkey"
+    )
+
+    op.drop_column(
+        "samples",
+        "hospital_id"
+    )
