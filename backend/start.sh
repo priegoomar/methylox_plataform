baf578bed98fd663f10c0fae781c96b724d5edca
priegoomar <161537962+priegoomar@users.noreply.github.com>
2026-08-06 18:15:27 -0600

    Update start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo "Updating Alembic state..."
-alembic stamp 002_add_patient_hospital_column
+echo "Running Alembic migrations..."
+alembic upgrade head
 echo "Starting METHYLOX API..."
 uvicorn app.main:app --host 0.0.0.0 --port 8000
